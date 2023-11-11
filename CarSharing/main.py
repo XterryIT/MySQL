597a03776b02ebd502dfed18289517a068509bf3
XterryIT <vptashka2@gmail.com>
2023-11-11 22:09:29 +0100

    Writing a basic code and connection to my database

---
+++ CarSharing/main.py
@@ -0,0 +1,36 @@
+import functions
+import pymysql
+from config import host,user,password,database
+
+
+def main():
+    try:
+    #conection to database
+        connection = pymysql.connect(
+        host = host,
+        port = 3306,
+        user = user,
+        password = password,
+        database = database,
+        cursorclass = pymysql.cursors.DictCursor 
+        )
+
+
+    #inplementing code
+        try:
+            pass
+
+
+    #closing the session
+        finally:
+            connection.close()
+
+
+#tracking and print the all kinds Erors
+    except Exception as ex:
+        print("NOT conection to", database)
+        print(ex)
+
+
+if __name__ == '__main__':
+    main()
